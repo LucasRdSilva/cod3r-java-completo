f87b0ed01fa333bb553dc093e4ff6ecf40c9ba56
LucasRdSilva <lucasrsilva42@gmail.com>
2021-06-16 22:16:25 -0300

    Aula 20

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,29 @@
 package fundamentos;
 
+/**
+ * 
+ * @author Lucas Rodrigues
+ * @since JDK1.0
+ */
 public class PrimeiroPrograma {
 
+	/**
+	 * ...
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		
+		//Uma sentença de código termina com ;
+		//Mais um comentário
+		//Fim
 		System.out.println("Primeiro programa Parte #01!");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3
+		 * 
+		 */
 		System.out.println("Primeiro programa Parte #02!");
 	}
 }
