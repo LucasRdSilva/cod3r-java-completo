dd6272192edcf5f6b6b0755ec605be7b81376432
LucasRdSilva <lucasrsilva42@gmail.com>
2021-07-15 22:13:12 -0300

    Aulas 24 à 31

---
+++ exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	
+	public static void main(String[] args) {
+		
+		double a = 2.3; //roxo/negrito - palavras reservadas (tipos primitivos no caso)
+		String s = "Bom dia X"; //Não primitivo / Não reservado
+		s = s.replace("X", "Senhora");
+		s = s.toUpperCase();
+		s = s.concat("!!!");
+		
+		System.out.println(s);
+		
+		String x = "Leo".toUpperCase();
+		System.out.println(x);
+		
+		String y = "Bom dia X"
+				.replace("X", "Gui")
+				.toUpperCase()
+				.concat("!!!");
+		System.out.println(y);
+		
+		//Tipos primitivos não tem  operador "."
+		int b = 3;
+		System.out.println(b);
+		
+	}
+
+}
---
+++ exercicios/src/fundamentos/PrimitivoVSObjeto.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class PrimitivoVSObjeto {
+	
+	public static void main(String[] args) {
+		
+		String s = new String("texto");
+		
+		s.toUpperCase();
+		
+		//Wrappers são a versão objeto dos tipos primitivos
+		
+	}
+
+}
